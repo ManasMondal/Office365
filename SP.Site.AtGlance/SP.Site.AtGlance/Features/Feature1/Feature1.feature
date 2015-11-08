dfe932c9f04b746b44f7b219cb0988490386a71f
ManasMondal <manas.mondal@hotmail.com>
2015-11-08 22:57:50 +0530

    initials App check-in

---
+++ SP.Site.AtGlance/SP.Site.AtGlance/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="f7c50685-4225-4097-bd07-39d87a8564ae" description="SharePoint App Feature" featureId="f7c50685-4225-4097-bd07-39d87a8564ae" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="SP.Site.AtGlance Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="c76ccc20-76f7-410c-99b4-ae426b45cddf" />
+    <projectItemReference itemId="7dc3d15e-2399-4978-9953-eedf56d04f2f" />
+    <projectItemReference itemId="1d84bf25-124e-43a8-b67c-73c2e4bc44b1" />
+    <projectItemReference itemId="e3c00a81-ffbf-4e3f-8f10-11f5a7f5dbca" />
+  </projectItems>
+</feature>
---
+++ SP.Site.AtGlance/SP.Site.AtGlance/Scripts/App.js
@@ -0,0 +1,43 @@
+﻿'use strict';
+
+ExecuteOrDelayUntilScriptLoaded(initializePage, "sp.js");
+
+/*function initializePage()
+{
+    var context = SP.ClientContext.get_current();
+    var user = context.get_web().get_currentUser();
+
+    // This code runs when the DOM is ready and creates a context object which is needed to use the SharePoint object model
+    $(document).ready(function () {
+        getUserName();
+    });
+
+    // This function prepares, loads, and then executes a SharePoint query to get the current users information
+    function getUserName() {
+        context.load(user);
+        context.executeQueryAsync(onGetUserNameSuccess, onGetUserNameFail);
+    }
+
+    // This function is executed if the above call is successful
+    // It replaces the contents of the 'message' element with the user name
+    function onGetUserNameSuccess() {
+        $('#message').text('Hello ' + user.get_title());
+    }
+
+    // This function is executed if the above call fails
+    function onGetUserNameFail(sender, args) {
+        alert('Failed to get user name. Error:' + args.get_message());
+    }
+} */
+
+var context = SP.ClientContext.get_current();
+var user = context.get_web().get_currentUser();
+var app = angular.module('myApp', []);
+app.controller('customersCtrl', function ($scope, $http) {
+    $http.get("http://www.w3schools.com/angular/customers.php")
+     .success(function (response) {
+         $scope.names = response.records;
+         
+     });
+    
+});
